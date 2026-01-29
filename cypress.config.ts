3964dbde2b93e9c55516a527b6c3035cc3708f80
icarodredd <icarog854@gmail.com>
2026-01-29 19:44:32 -0300

    build: configure cypress to use studio mode

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
+import createBundler from '@bahmutov/cypress-esbuild-preprocessor';
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on(
+        'file:preprocessor',
+        createBundler({ sourcemap: 'inline', tsconfig: 'tsconfig.json' }),
+      );
+      return config;
     },
   },
 });
